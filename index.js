c32403f91d310f0a9c005a166712697c0eb4a410
mdawlad29 <mdawladbdinfo@gmail.com>
2024-05-23 18:06:25 +0600

    Test row server

---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// const inputTxt = fs.readFileSync("./txt/input.txt", "utf-8");
+// const outputTxt = `this is output text: ${inputTxt}.\nCreate on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", outputTxt);
+
+// Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const server = http.createServer((req, res) => {
+  const path = req.url;
+
+  // Overview page
+  if (path === "/" || path === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+
+    // Product page
+  } else if (path === "/product") {
+    res.end("this is the product!");
+
+    // Product Api
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    // Not found page
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000!");
+});
